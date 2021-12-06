ca34edf742b57095a9fe77f8c499a00ebb4a0818
analyst-in-the-wild <copyrightsplashphotography@yahoo.co.uk>
2021-12-06 12:53:15 +0000

    roxygen documentation

--- praise_and_insults.R
+++ praise_and_insults.R
@@ -1,7 +1,20 @@
+#' Geussing who staff is!
+#'
+#' @description when you run the function with a purpose and lines of code it will guess who the staff member is between Zoe, Chris, Milan and Andreas
+#'
+#' @param purpose either Stats, Data or Text Mining
+#' @param lines_of_code integar variable
+#'
+#' @return
+#' @export
+#'
+#' @examples praise_function(purpose = "data", lines_of_code = 200)
+#'  "Zoe is outstanding"
+#'
+#'
+#'
 praise_function <- function(purpose = NULL,
                             lines_of_code = NULL) {
-
-  output <- paste(allies, praise::praise("is ${adjective}"))
 
   if(purpose == "text mining" && lines_of_code > 50) {
 
@@ -24,7 +37,7 @@ praise_function <- function(purpose = NULL,
     allies <- c("Milan")
 
   }
-  output
+  paste(allies, praise::praise("is ${adjective}"))
 }
 
-praise_function(purpose = "stats", lines_of_code = 200)
+praise_function(purpose = "data", lines_of_code = 200)

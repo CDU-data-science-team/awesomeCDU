84b68844ae438d2d673f1dcd58a61a9df8651c59
analyst-in-the-wild <copyrightsplashphotography@yahoo.co.uk>
2021-12-06 12:42:58 +0000

    stop being nice to the traitor Milan!

--- praise_and_insults.R
+++ praise_and_insults.R
@@ -1,6 +1,30 @@
-nice_people <- paste(nice_team_members, praise("have been acceptable - You are ${adjective}"))
+praise_function <- function(purpose = NULL,
+                            lines_of_code = NULL) {
 
-not_nice_people <- paste(less_nice_team_member, praise("has not been acceptable - You are not ${adjective}"))
+  output <- paste(allies, praise::praise("is ${adjective}"))
 
-nice_people
-not_nice_people
+  if(purpose == "text mining" && lines_of_code > 50) {
+
+    allies <- c("Andreas")
+
+  }
+
+  else if(purpose == "stats" && lines_of_code > 50) {
+
+    allies <- c("Chris")
+
+  }
+
+  else if(purpose == "data" && lines_of_code > 50) {
+
+    allies <- c("Zoe")
+
+  } else {
+
+    allies <- c("Milan")
+
+  }
+  output
+}
+
+praise_function(purpose = "stats", lines_of_code = 200)
